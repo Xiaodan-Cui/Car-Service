97819881762f525f4ad0a01eb7ef66a203ee276c
xiaodan cui <cxd@xiaodan-MacBook-Pro.local>
2021-05-13 21:39:18 -0700

    fdf

---
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PriceApiUnitTest.java
@@ -0,0 +1,37 @@
+package com.udacity.pricing;
+
+import com.udacity.pricing.api.PricingController;
+import com.udacity.pricing.service.PricingService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PricingController.class)
+public class PriceApiUnitTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    PricingService pricingService;
+
+    @Test
+    public void getA() throws Exception{
+        mockMvc.perform(get("/services/price?vehicleId=1"))
+                .andExpect(status().isOk());
+
+
+
+    }
+
+
+}
---
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/Config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.udacity.vehicles.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Find Your Dream Car Api")
+                .description("microservice, JUnit, Swagger, graphQL")
+                .termsOfServiceUrl("http://localhost:8080")
+                .contact(new Contact(
+                "Xiaodan Cui","dfdfdf.com","ddd@dd.con"))
+                .version("1.0.0")
+                .build();
+    }
+}
